03de4d5718414fe6068d95ccb00f01a1dad3a7af
yuooo <j3ss0u@gmail.com>
2017-04-05 17:18:13 -0500

    ++ greedy algo

--- setcover.py
+++ setcover.py
@@ -28,6 +28,8 @@ def greedy(h_elements, l_sets):
     while h_elements:
         best_ratio = inf
         best_index = 0
+
+        # find argmin
         for i_set, sett in enumerate(l_sets):
             weight_set, h_element_in_the_set = sett
             new_ratio = float(weight_set) / len(h_element_in_the_set)
@@ -35,12 +37,24 @@ def greedy(h_elements, l_sets):
                 best_ratio = new_ratio
                 best_index = i_set
         best_weight, best_set = l_sets[best_index]
+
+        # Remove best set from the list of possible sets
         del l_sets[best_index]
         total_weight += best_weight
+
+        # Remove newly covered elements to the list of elements to cover
         for element_now_covered in best_set:
             del h_elements[element_now_covered]
+
+        # Remove newly covered elements from the sets left
+        for sett in l_sets:
+            _ , h_set = sett 
+            for element_now_covered in best_set:
+                del h_set[element_now_covered]
+    return total_weight
  
 
 
        
+                
                 
