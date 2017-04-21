fb6138d564ae9aef0a5f34871e408b02812af629
yuooo <j3ss0u@gmail.com>
2017-04-21 18:13:00 -0500

    ++ plotting function for comparing set cover value

--- comparealgorithms.py
+++ comparealgorithms.py
@@ -8,113 +8,54 @@ Created on Fri Apr 14 12:05:27 2017
 import setcover
 import data_set
 import numpy as np
-
-#%% A few examples to try stuff out
-
-# instance 1
-element1 = {'a': 0, 'b': 0, 'c': 0}
-sets1 = [(2, {'a':0}), \
-(10, {'b':0, 'c':0}), \
-(3, {'a':0, 'c':0}), \
-(8, {'b':0}), \
-(8, {'b':0, 'c':0})]
-#setcover.usual_greedy(element1, sets1, False)
-
-element1 = {'a': 0, 'b': 0, 'c': 0}
-sets1 = [(2, {'a':0}), \
-(10, {'b':0, 'c':0}), \
-(3, {'a':0, 'c':0}), \
-(8, {'b':0}), \
-(8, {'b':0, 'c':0})]
-#setcover.heuristic_frequency1(element1, sets1, False)
-
-element1 = {'a': 0, 'b': 0, 'c': 0}
-sets1 = [(2, {'a':0}), \
-(10, {'b':0, 'c':0}), \
-(3, {'a':0, 'c':0}), \
-(8, {'b':0}), \
-(8, {'b':0, 'c':0})]
-#setcover.heuristic_frequency2(element1, sets1, False)
-
-element1 = {'a': 0, 'b': 0, 'c': 0}
-sets1 = [(2, {'a':0}), \
-(10, {'b':0, 'c':0}), \
-(3, {'a':0, 'c':0}), \
-(8, {'b':0}), \
-(8, {'b':0, 'c':0})]
-#setcover.heuristic_frequency3(element1, sets1, False)
-
-element1 = {'a': 0, 'b': 0, 'c': 0}
-sets1 = [(2, {'a':0}), \
-(10, {'b':0, 'c':0}), \
-(3, {'a':0, 'c':0}), \
-(8, {'b':0}), \
-(8, {'b':0, 'c':0})]
-#setcover.heuristic_frequency4(element1, sets1, False)
-
-# instance 2
-element1 = {'a': 0, 'b': 0, 'c': 0, 'd':0}
-sets1 = [(8, {'a':0, 'b':0}), \
-(7, {'c':0, 'd':0}), \
-(9, {'b':0, 'c':0, 'd':0})]
-#setcover.usual_greedy(element1, sets1, False)
-
-element1 = {'a': 0, 'b': 0, 'c': 0, 'd':0}
-sets1 = [(8, {'a':0, 'b':0}), \
-(7, {'c':0, 'd':0}), \
-(9, {'b':0, 'c':0, 'd':0})]
-setcover.heuristic_frequency1(element1, sets1, True)
-
-element1 = {'a': 0, 'b': 0, 'c': 0, 'd':0}
-sets1 = [(8, {'a':0, 'b':0}), \
-(7, {'c':0, 'd':0}), \
-(9, {'b':0, 'c':0, 'd':0})]
-#setcover.heuristic_frequency2(element1, sets1, False)
-
-element1 = {'a': 0, 'b': 0, 'c': 0, 'd':0}
-sets1 = [(7, {'a':0, 'b':0}), \
-(7, {'c':0, 'd':0}), \
-(9, {'b':0, 'c':0, 'd':0})]
-#setcover.heuristic_frequency3(element1, sets1, False)
-
-element1 = {'a': 0, 'b': 0, 'c': 0, 'd':0}
-sets1 = [(8, {'a':0, 'b':0}), \
-(7, {'c':0, 'd':0}), \
-(9, {'b':0, 'c':0, 'd':0})]
-#setcover.heuristic_frequency4(element1, sets1, False)
-
+import matplotlib.pyplot as plt
+from preprocessing import *
+import matplotlib.cm as cm
+import pandas as pd
 # DATA SETS: http://people.brunel.ac.uk/~mastjjb/jeb/orlib/scpinfo.html
 
 #%% General functions
-def run_experiment(path, verbose):
+def setcover_value(path, verbose):
 
-	total_weights = []
+    total_weights = []
 
-	element1, sets1 = create_data_set(path)
-	total_weights.append(setcover.usual_greedy(element1, sets1, verbose))
-
-	element1, sets1 = create_data_set(path)
-	total_weights.append(setcover.heuristic_frequency1(element1, sets1, verbose))
+    element1, sets1 = create_data_set(path)
+    total_weights.append(setcover.heuristic_frequency1(element1, sets1, verbose))
 	
-	element1, sets1 = create_data_set(path)
-	total_weights.append(setcover.heuristic_frequency2(element1, sets1, verbose))
-	
-	element1, sets1 = create_data_set(path)
-	total_weights.append(setcover.heuristic_frequency3(element1, sets1, verbose))
-	
-	element1, sets1 = create_data_set(path)
-	total_weights.append(setcover.heuristic_frequency4(element1, sets1, verbose))
+    element1, sets1 = create_data_set(path)
+    total_weights.append(setcover.heuristic_frequency2(element1, sets1, verbose))
+    
+    element1, sets1 = create_data_set(path)
+    total_weights.append(setcover.heuristic_frequency3(element1, sets1, verbose))
 
-	return total_weights
+    element1, sets1 = create_data_set(path)
+    total_weights.append(setcover.heuristic_frequency4(element1, sets1, verbose))
+
+    element1, sets1 = create_data_set(path)
+    total_weights.append(setcover.usual_greedy(element1, sets1, verbose))
+
+    return total_weights
+
+def run_benchmark():
+    results = np.array([])
+    datasets = range(41,50) + range(51,60) + range(61,66) + [410]
+    for dataset in datasets:
+        path = "data_sets/scp"+str(dataset)+".txt"
+        total_weights = setcover_value(path, False)
+        results = np.vstack((results, total_weights)) if len(results) else total_weights 
+        with open("Output.txt", "w") as text_file:
+            text_file.write(str(results))
+        
+    df = pd.DataFrame(results)
+    df.plot(colormap = 'rainbow')
+         
+    return results
 
 
 
-results = np.array([])
-datasets = range(41,50) + range(51,60) + range(61,66) + [410]
-for dataset in datasets:
-	path = "data_sets/scp"+str(dataset)+".txt"
-	total_weights = data_set.run_experiment(path, False)
-	results = np.vstack((results, total_weights)) if len(results) else total_weights 
-print results
+b = run_benchmark()
+
+
+
 
 
